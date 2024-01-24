5d9ed5c3c5548675a3745940b5b3f465e5da4dcc
rsbqr <rsbqr.coder@gmail.com>
2024-01-24 16:06:30 +0700

    edited home dan bikes

--- pages/1_Bike_Data_Demo.py
+++ pages/1_Bike_Data_Demo.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-st.set_page_config(page_title="Bike Data Demo", page_icon="🚲")
+st.set_page_config(page_title="Bikes Data Demo", page_icon="🚲")
 # st.markdown("# Bike Data Demo")
 # st.sidebar.header("Bike Data Demo")
 
