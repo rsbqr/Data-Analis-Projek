b235cd29606050f46a4522c2b03694fd938f3238
Raihan Sabiq Rabbani <43677172+rsbqr@users.noreply.github.com>
2024-09-16 14:41:09 +0700

    hapus Home.py

--- home.py
+++ home.py
@@ -39,7 +39,8 @@ def run():
         
         👈 _Silahkan pilih menu di sidebar_ untuk mengaksesnya
         
-        :red[Note] Jika akses menggunakan _mobile / handphone / gawai_
+        :red[Note] Jika akses menggunakan _mobile / handphone / gawai
+        _
         
         _Menu_ dapat diakses dengan klik _button_ seperti (>) pada :blue[pojok kiri atas]
 
